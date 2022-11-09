e709b9f28e9c808235923f1c07597154a13863d8
fauzul-yahya <fauzulyahya@gmail.com>
2022-11-09 11:15:13 +0700

    tugas1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Fauzul from './tugas/Fauzul'
+
+const App = () => {
+  return (
+    <div>
+      <Fauzul />
+    
+    </div>
+  )
+}
+
+export default App
+
+
+
